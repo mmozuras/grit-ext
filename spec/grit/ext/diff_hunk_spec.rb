6de40e60f23fc1ecb59170a76b9d9941c17abff7
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2013-07-01 21:28:04 +0300

    More specs for DiffHunk

--- spec/grit/ext/diff_hunk_spec.rb
+++ spec/grit/ext/diff_hunk_spec.rb
@@ -4,12 +4,9 @@ module Grit
   describe DiffHunk do
     let(:diff_hunk) { DiffHunk.new(header, hunk.split("\n")) }
 
-    describe '#lines' do
-      subject { diff_hunk.lines }
-
-      context 'hunk with two added lines' do
-        let(:header) { DiffHeader.new('@@ -10,4 +10,6 @@') }
-        let(:hunk) { <<-eos
+    shared_context 'hunk with two added lines' do
+      let(:header) { DiffHeader.new('@@ -10,4 +10,6 @@') }
+      let(:hunk) { <<-eos
    if something
      one
 +    two
@@ -17,13 +14,38 @@ module Grit
      four
    end
 eos
-        }
+      }
+    end
 
-        its(:count) { should == 6 }
-        specify { subject[3].content.should == '    three' }
-        specify { subject[3].status.should == :added }
-        specify { subject[3].position.should == 13 }
-      end
+    describe '#lines' do
+      include_context 'hunk with two added lines'
+      subject { diff_hunk.lines }
+
+      its(:count) { should == 6 }
+      specify { subject[3].content.should == '    three' }
+      specify { subject[3].status.should == :added }
+      specify { subject[3].position.should == 13 }
+    end
+
+    describe '#added' do
+      include_context 'hunk with two added lines'
+      subject { diff_hunk.added }
+
+      its(:count) { should == 2 }
+    end
+
+    describe '#removed' do
+      include_context 'hunk with two added lines'
+      subject { diff_hunk.removed }
+
+      its(:count) { should == 0 }
+    end
+
+    describe '#unchanged' do
+      include_context 'hunk with two added lines'
+      subject { diff_hunk.unchanged }
+
+      its(:count) { should == 4 }
     end
   end
 end
